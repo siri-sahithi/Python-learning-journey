246e391732825bf348b8bbd725365a65996c3d99
Satyala Siri Sahithi <sirisahithi00@gmail.com>
2026-08-27 23:49:52 +0530

    Add files via upload

---
+++ NumPy/arr-vector multiplication-np.py
@@ -0,0 +1,7 @@
+#create a numpy program for multiplication of two vectors 
+import numpy as np
+arr1=np.array([2,4,6,8])
+arr2=np.array([1,3,5,7])
+print("First Vector ", arr1)
+print("second Vector ", arr2)
+print("Multiplication of two vectors\n",arr1*arr2)
