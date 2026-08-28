53434cdd808526b3311bac2ce4c869a1ea2de959
Satyala Siri Sahithi <sirisahithi00@gmail.com>
2026-08-29 00:01:03 +0530

    Add files via upload

---
+++ NumPy/mat-rc-minmax-np.py
@@ -0,0 +1,16 @@
+#practice questions 
+#Write numpy program to find max and min elements in a matrix
+import numpy as np
+mat=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print("The maximum elements in array is :",np.max(mat))
+print("The minimum elements in array is :",np.min(mat))
+#write a numpy program to find max & min elements row wise in given matrix
+print("The maximum elements in array is :",np.max(mat,axis=0))
+
+
+
+
+
+
+#write a numpy program to find max & min elements column wise in given matrix
+
