221735a0ef316e722c7a806523983e56fbf23e19
Satyala Siri Sahithi <sirisahithi00@gmail.com>
2026-09-09 23:16:01 +0530

    Add files via upload

---
+++ Basic Programs/dict-1.py
@@ -0,0 +1,8 @@
+dct = {'one': 'two', 'three': 'one', 'two': 'three'}
+v = dct['three']
+
+for k in range(len(dct)):
+    v = dct[v]
+
+print(v)
+
---
+++ Basic Programs/housing.py
@@ -0,0 +1,3 @@
+import pandas as pd
+data = pd.read_csv("house_selling_dataset_unpreprocessed.csv")
+print(data)
