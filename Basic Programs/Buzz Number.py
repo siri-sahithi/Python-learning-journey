0a7ea067c6706b96619cb737b84e62bec866b475
Siri Sahithi Satyala <sirisahithi00@gmail.com>
2026-09-20 23:51:06 +0530

    Add files via upload

---
+++ Basic Programs/Buzz Number.py
@@ -0,0 +1,8 @@
+n=int(input("Enter a Number"))
+if(n%7==0):
+    print("The given number ",n," a Buzz number")
+d=n%10
+elif(d==7):
+    print("The given number ",n," a Buzz number")
+else:
+    print("The given number ",n," is not a Buzz number")
