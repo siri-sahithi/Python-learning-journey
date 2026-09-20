5b9a824582cf794126fcc1396ab386c7c1da247b
Siri Sahithi Satyala <sirisahithi00@gmail.com>
2026-09-21 00:00:03 +0530

    Add files via upload

---
+++ Pandas/series3-pd.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+arr1 = np.array([10, 11, 12, 13, 14, 15])
+print("Given array is: ", arr1)
+print(type(arr1))
+S1 = pd.Series(arr1)
+print(S1)
+print(type(S1))
